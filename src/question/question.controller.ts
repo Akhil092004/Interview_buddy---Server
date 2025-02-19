80f4059c6ab422f8f61572d530bca86fd882ef70
Akhil092004 <akhiljain092004@gmail.com>
2025-02-19 22:05:51 +0530

    implement question retrieval and deletion features; enhance error handling in question service

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -1,9 +1,9 @@
 
 
-import { Body, Controller,Post, UseGuards } from '@nestjs/common';
+import { Body, Controller,Delete,Get,Param,Post, Query, UseGuards } from '@nestjs/common';
 import { QuestionService } from './question.service';
 import { QuestionDto } from './dto';
-import { UserGuard } from 'src/user/user.guard';
+import { UserGuard } from './../user/user.guard';
 import { Question } from './schemas/question.schema';
 
 @Controller('question')
@@ -16,6 +16,28 @@ export class QuestionController {
         return await this.QuestionService.createQuestion(dto);
     }
     
+    @UseGuards(UserGuard)
+    @Get()
+    async getQuestions(@Query() dto:{topic:string,diffifulty?:string}) {
+        return await this.QuestionService.getQuestions(dto);
+    }
 
+    @UseGuards(UserGuard)
+    @Get("user/:id")
+    async getQuestionsByUser(@Param() param:{id:string}){
+        return await this.QuestionService.getQuestionsByUser(param.id);
+    }
+
+    @UseGuards(UserGuard)
+    @Get(':id')
+    async getQuestionById(@Param() param:{id:string}){
+        return await this.QuestionService.getQuestionById(param.id);
+    }
+
+    @UseGuards(UserGuard)
+    @Delete(':id')
+    async deleteQuestion(@Param() param:{id:string}){
+        return await this.QuestionService.deleteQuestion(param.id);
+    }
 
 }
--- src/question/question.service.ts
+++ src/question/question.service.ts
@@ -1,4 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  HttpException,
+  HttpStatus,
+  BadRequestException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Question } from './schemas/question.schema';
 import mongoose, { Types } from 'mongoose';
@@ -6,28 +12,89 @@ import { QuestionDto } from './dto';
 
 @Injectable()
 export class QuestionService {
-    constructor(
-        @InjectModel(Question.name)
-        private questionModel:mongoose.Model<Question>
-    ){}
+  constructor(
+    @InjectModel(Question.name)
+    private questionModel: mongoose.Model<Question>,
+  ) {}
 
+  async createQuestion(dto: QuestionDto): Promise<Question> {
+    try {
+      const {
+        questionText,
+        topic,
+        difficulty = 'Medium',
+        createdBy,
+        hints = [],
+      } = dto;
 
-    async createQuestion(dto:QuestionDto):Promise<Question>{
-        try {
-            const {questionText,topic,difficulty = "Medium",createdBy,hints =[]} = dto;
+      const question = new this.questionModel({
+        questionText,
+        topic,
+        difficulty,
+        createdBy: new Types.ObjectId(createdBy),
+        hints,
+      });
 
-            const question = new this.questionModel({
-                questionText,
-                topic,
-                difficulty,
-                createdBy : new Types.ObjectId(createdBy),
-                hints
-            })
-
-            return await question.save();
-
-        } catch (error) {
-            throw new Error(error instanceof Error ? error.message : String(error));
-        }
+      return await question.save();
+    } catch (error) {
+      throw new Error(error instanceof Error ? error.message : String(error));
     }
+  }
+
+  async getQuestions({
+    topic,
+    difficulty,
+  }: {
+    topic: string;
+    difficulty?: string;
+  }) {
+    try {
+      const filter: { topic: string; difficulty?: string } = { topic };
+
+      if (difficulty) filter.difficulty = difficulty;
+
+      return await this.questionModel.find(filter);
+    } catch (error) {
+      throw new NotFoundException(error);
+    }
+  }
+
+  async getQuestionsByUser(id: string): Promise<Question[]> {
+    try {
+      const objectId = new Types.ObjectId(id);
+
+      const questions = await this.questionModel.find({ createdBy: objectId });
+
+      return questions;
+    } catch (error) {
+      throw new HttpException((error as Error).message, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async getQuestionById(id: string): Promise<Question> {
+    try {
+      const ques = await this.questionModel.findById(id);
+
+      if (!ques)
+        throw new NotFoundException('No questions found with given id');
+
+      return ques;
+    } catch (error) {
+      throw new NotFoundException(error);
+    }
+  }
+
+  async deleteQuestion(id: string): Promise<{ message: string }> {
+    try {
+      const deletedQues = await this.questionModel.findByIdAndDelete(id);
+
+      if (!deletedQues)
+        throw new NotFoundException(`Question with id = "${id}" not found`);
+
+      //make sure to delete the corresponding answer also
+      return { message: 'Question deleted Successfully' };
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
 }
