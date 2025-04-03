cebeb61125110d68a65503b1409f892c9c5814c3
Akhil092004 <akhiljain092004@gmail.com>
2025-04-03 22:55:30 +0530

    add AiModule to app module imports for enhanced functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,14 @@ import { AppService } from './app.service';
 import { QuestionModule } from './question/question.module';
 import { AnswerModule } from './answer/answer.module';
 import { UserModule } from './user/user.module';
+import { AiModule } from './ai/ai.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     QuestionModule, 
     AnswerModule, 
-    MongooseModule.forRoot(process.env.MONGODB_URI ?? ''), UserModule, // Ensure proper fallback
+    MongooseModule.forRoot(process.env.MONGODB_URI ?? ''), UserModule, AiModule, 
   ],
   controllers: [AppController],
   providers: [AppService],
